c664b89342762522e6e49170ea77973f842c185e
sohamviradiya <sohamviradiya2003@gmail.com>
2024-11-21 18:23:31 +0530

    update preprocess notebook to download Russell data and enhance data fetching in std_methods; add SP500, DJIA, and Russell metrics to results.txt

--- std_methods.py
+++ std_methods.py
@@ -4,7 +4,10 @@ from scipy.optimize import minimize
 
 def fetch_data(tickers):
     files = [f'./data/processed/{asset}.csv' for asset in tickers]
-    return [pd.read_csv(file) for file in files]
+    spy = pd.read_csv('./data/spy.csv')
+    djia = pd.read_csv('./data/djia.csv')
+    russell = pd.read_csv('./data/russell.csv')
+    return [pd.read_csv(file) for file in files],spy,djia,russell
 
 def equal_weights(train_data,eval_data):
     relative_eval_data = eval_data/eval_data[0]
@@ -34,8 +37,10 @@ def risk_parity(train_data,eval_data):
     return relative_eval_data @ w
 
 def std_methods(tickers,train_date_start,train_date_end,eval_date_start,eval_date_end):
-    data = fetch_data(tickers)
-    train_data = np.array([prices[(prices['date']>=train_date_start) & (prices['date']<=train_date_end)]['close'].values for prices in data]).T
-    eval_data = np.array([prices[(prices['date']>=eval_date_start) & (prices['date']<=eval_date_end)]['close'].values for prices in data]).T
-    
-    return equal_weights(train_data,eval_data),min_risk(train_data,eval_data),risk_parity(train_data,eval_data)
+    assets,spy,djia,russell = fetch_data(tickers)
+    train_data = np.array([prices[(prices['date']>=train_date_start) & (prices['date']<=train_date_end)]['close'].values for prices in assets]).T
+    eval_data = np.array([prices[(prices['date']>=eval_date_start) & (prices['date']<=eval_date_end)]['close'].values for prices in assets]).T
+    spy_eval = spy[(spy['date']>=eval_date_start) & (spy['date']<=eval_date_end)]['close'].values
+    djia_eval = djia[(djia['date']>=eval_date_start) & (djia['date']<=eval_date_end)]['close'].values
+    russell_eval = russell[(russell['date']>=eval_date_start) & (russell['date']<=eval_date_end)]['close'].values
+    return equal_weights(train_data,eval_data),min_risk(train_data,eval_data),risk_parity(train_data,eval_data),spy_eval/spy_eval[0],djia_eval/djia_eval[0],russell_eval/russell_eval[0]
